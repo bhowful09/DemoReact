c854825cf7f5ebd3007e2824afe5a3d4724608cc
Benjamin Howard <benjamin.howard.09@gmail.com>
2019-01-17 01:29:23 -0500

    updated display of multiple movies

--- src/Movie.js
+++ src/Movie.js
@@ -7,42 +7,80 @@ class Movie extends Component{
         super();
         this.state = {
             movies: [],
+            searchTerm: "spider man",
+            error: false
         };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.onNameChange = this.onNameChange.bind(this);
     }
 
     componentDidMount(){
-        fetch('https://www.omdbapi.com/?s=bond&apikey=51157d3')
+        fetch('https://www.omdbapi.com/?s=' + this.state.searchTerm + '&apikey=51157d3')
         .then(results => results.json())
         .then(data => {
             this.setState({movies: data.Search});
         })
     }
 
+    handleClick() {
+        fetch('https://www.omdbapi.com/?s=' + this.state.searchTerm + '&apikey=51157d3')
+        .then(results => results.json())
+        .then(data => {
+            if(data.Error){
+                this.setState(
+                    {
+                        movies: [],
+                        error: true,
+                        searchTerm: this.state.searchTerm
+                    });
+            }
+            else{
+                this.setState({movies: data.Search});
+            }
+        })
+      }
+
+      onNameChange(event) {
+        this.setState({
+            searchTerm: event.target.value,
+            movies: this.state.movies,
+            error: false,
+        });
+      }
+
     render(){
         return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Title</th>
-                        <th>Year</th>
-                        <th>Type</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.movies.map(movie => (
-                        <tr key={movie.imdbID}>
-                            <td><img className="Poster" src={movie.Poster === "N/A" ? "https://s18672.pcdn.co/wp-content/uploads/2018/01/Movie.jpg" : movie.Poster}></img></td>
-                            <td>{movie.Title}</td>
-                            <td>{movie.Year}</td>
-                            <td>{movie.Type}</td>
-                        </tr>
+            <div>
+                <h1 className='header'>Search for Movies, TV Series and More!</h1>
+                <div className="center container">
+                    <div className="center">
+                        <div>
+                            <input className="searchBox" type="text" value={this.state.searchTerm} onChange={this.onNameChange}/>
+                        </div>
+                    </div>
+                    
+                    <div className="center">
+                        <div>
+                            <input className="btn btn-primary" type="button" value="Search By Title" onClick={this.handleClick} />
+                        </div>
+                    </div>
+                    {
+                        this.state.movies.map(movie => (
+                        <div key={movie.imdbID} className="movie card center mx-auto">
+                            <div className="movie top">
+                                <img className="poster" alt={movie.Title + " poster"} src={movie.Poster === "N/A" ? "https://s18672.pcdn.co/wp-content/uploads/2018/01/Movie.jpg" : movie.Poster}></img>
+                            </div>
+                            <div className="movie bottom">
+                                <p className="h2">{movie.Title}</p>
+                                <p>({movie.Year}) {movie.Type}</p>
+                            </div>
+                        </div>
                     ))}
-                </tbody>
-            </table>
+                </div>
+            </div>
         )
     }
 }
-
 
 export default Movie;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Customer from './Customer';
 import Movie from './Movie';
 import * as serviceWorker from './serviceWorker';
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
 ReactDOM.render(<Movie/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
